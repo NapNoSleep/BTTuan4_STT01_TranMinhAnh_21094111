2f509d303d0490d993577e123d1265072d39f992
NapNoSleep <minhanhnb357@gmail.com>
2024-10-09 13:25:31 +0700

    hello
    hello

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Screen_01 from './screens/Screen_01';
+import Screen_03 from './screens/Screen_03';
+import Screen_04 from './screens/Screen_04';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Screen_01">
+        <Stack.Screen name="Screen_01" component={Screen_01} options={{ title: 'Login' }} />
+        <Stack.Screen name="Screen_03" component={Screen_03} options={{ title: 'Forgot Password' }} />
+        <Stack.Screen name="Screen_04" component={Screen_04} options={{ title: 'TikTok OK' }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
